9d4bde7815250d412cd8a9fad8c65f7850755f54
ben-rochlin <brochlin91@gmail.com>
2021-01-10 16:35:11 +1100

    begin layout for menu bar and imported logo

---
+++ client/src/components/menuBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+// import { Nav, Dropdown } from 'rsuite';
+import 'rsuite/dist/styles/rsuite-default.css';
+import './menuBar.scooped.scss'
+import '../../public/LOGOFINAL.svg'
+
+const MenuBar = () => {
+  return(
+<div className="outerContainer">
+<div className="inner">
+<img src="LOGOFINAL.svg" alt="Kiwi standing on oval" className="sizing"/>
+</div>
+<div className="inner"></div>
+</div>
+  )
+}
+
+export default MenuBar
---
+++ client/src/pages/home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Title from "../components/title";
+import MenuBar from '../components/menuBar.jsx'
+
+
+const Home = () => {
+  return (
+    <div>
+      <MenuBar />
+      {/* <Title /> */}
+    </div>
+  );
+}
+
+export default Home
