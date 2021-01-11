e187ee3e434976fa0bf79bc12ebd0c2fea174a3d
ben-rochlin <brochlin91@gmail.com>
2021-01-11 17:27:30 +1100

    minor add

--- client/src/route/routes.js
+++ client/src/route/routes.js
@@ -5,6 +5,7 @@ import Home from "../pages/home";
 import CategoriesPage from "../pages/categoriesPage"
 import PublicationsPage from "../pages/publicationsPage"
 import LoginPage from "../pages/loginPage"
+import SigninPage from "../pages/signinPage"
 
 
 export default function Routes() {
@@ -14,7 +15,7 @@ export default function Routes() {
       <Route exact path="/" exact component={Home} />
       <Route exact path="/categories" exact component={CategoriesPage} />
       <Route exact path="/publications" exact component={PublicationsPage} />
-      <Route exact path="/login" exact component={LoginPage} />
+      <Route exact path="/signin" exact component={SigninPage} />
       </Switch>
     </Router>
   );
