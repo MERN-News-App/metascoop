f63c16ba41048ec761b713ff830ae8be54ebb754
ben-rochlin <brochlin91@gmail.com>
2021-01-12 00:03:35 +1100

    image finaaly sorted

--- client/src/components/contentCard.jsx
+++ client/src/components/contentCard.jsx
@@ -3,12 +3,13 @@ import './contentCard.scoped.scss'
 
 const title = "Lakers' new starter becomes their new star, helping LeBron James hold off the Bulls"
 
+
 const ContentCard = () => {
   return(
       <div className="wrapper">
         <div className="outerContainer">
           <div className="title">{title}</div>
-          <h1 className="hello">hello there</h1>
+          <img src="https://ca-times.brightspotcdn.com/dims4/default/4506919/2147483647/strip/true/crop/4226x2377+0+220/resize/1200x675!/quality/90/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F98%2Fc5%2F2c7158b54a778ee3e9119cfe4517%2Fbulls-lakers-basketball-82900.jpg" alt="" className="hello"/>
         </div>
       </div>
   )
