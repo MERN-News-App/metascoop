d125ad959ca3f17ffc85c0203dc8a61f154b1ed9
ben-rochlin <brochlin91@gmail.com>
2021-01-11 23:42:41 +1100

    start on grid layout for content card

--- client/src/components/contentCard.jsx
+++ client/src/components/contentCard.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './contentCard.scoped.scss'
 
+const title = "Lakers' new starter becomes their new star, helping LeBron James hold off the Bulls"
+
 const ContentCard = () => {
   return(
-      <div className="outerContainer">
-          <h1>hello there</h1>
+      <div className="wrapper">
+        <div className="outerContainer">
+          <div className="title">{title}</div>
+          <h1 className="hello">hello there</h1>
+        </div>
       </div>
   )
 }
