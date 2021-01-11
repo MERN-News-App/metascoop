181b556eb47f75787f20e8d453702546694ec466
ben-rochlin <brochlin91@gmail.com>
2021-01-12 00:25:59 +1100

    new div for inline icons

--- client/src/components/contentCard.jsx
+++ client/src/components/contentCard.jsx
@@ -5,13 +5,19 @@ const title = "Lakers' new starter becomes their new star, helping LeBron James 
 
 
 const ContentCard = () => {
-  return(
-      <div className="wrapper">
-        <div className="outerContainer">
-          <div className="title">{title}</div>
-          <img src="https://ca-times.brightspotcdn.com/dims4/default/4506919/2147483647/strip/true/crop/4226x2377+0+220/resize/1200x675!/quality/90/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F98%2Fc5%2F2c7158b54a778ee3e9119cfe4517%2Fbulls-lakers-basketball-82900.jpg" alt="" className="hello"/>
+  return (
+    <div className="wrapper">
+      <div className="outerContainer">
+          <div className="tdiv">
+        <div className="title">{title}
+        <span className="cats">cat</span>
+        </div>  
+            </div>
+            <img src="https://ca-times.brightspotcdn.com/dims4/default/4506919/2147483647/strip/true/crop/4226x2377+0+220/resize/1200x675!/quality/90/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F98%2Fc5%2F2c7158b54a778ee3e9119cfe4517%2Fbulls-lakers-basketball-82900.jpg" alt="" className="hello" />
+          </div>
         </div>
-      </div>
+      
+    
   )
 }
 
