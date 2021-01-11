a495f6437c58512214959be4d43320a0887c606d
ben-rochlin <brochlin91@gmail.com>
2021-01-11 19:14:29 +1100

    added material ui and searchbar

--- client/src/components/menuBar.jsx
+++ client/src/components/menuBar.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch, faUser } from '@fortawesome/free-solid-svg-icons'
-
-
-// import { Nav, Dropdown } from 'rsuite';
 import 'rsuite/dist/styles/rsuite-default.css';
 import './menuBar.scoped.scss'
 import '../../public/LOGOFINAL.svg'
+// import { AutoComplete, InputGroup, Icon } from 'rsuite';
+
+import SearchBar from './searchbar.jsx'
+
+
+const styles = {
+    width: 100,
+    marginBottom: 10
+  };
+  
+  const data = [
+    'HYPER Advertiser',
+    'HYPER Web Analytics',
+    'HYPER Video Analytics',
+    'HYPER DMP',
+    'HYPER Ad Serving',
+    'HYPER Data Discovery'
+  ];
+
+
+// import { Nav, Dropdown } from 'rsuite';
 
 const MenuBar = () => {
     return (
@@ -15,14 +33,16 @@ const MenuBar = () => {
                 <img src="LOGOFINAL.svg" alt="Kiwi standing on oval" className="logo" />
                 <h1 className="metascoop">METASCOOP</h1>
             </div>
+            <div className="innerMiddle">
+                <div className="searchbar">
+                 <SearchBar />
+                </div>
+            </div>
             <div className="innerRight">
                 <h1 className="item">login</h1>
                 <h1 className="item">Signup</h1>
                 <span className="icon">
                     <FontAwesomeIcon icon={faUser} size="3x" className="icons" />
-                </span>
-                <span className="icon">
-                    <FontAwesomeIcon icon={faSearch} size="3x" className="icons" />
                 </span>
             </div>
         </div>
---
+++ client/src/components/searchbar.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: '4px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+  divider: {
+    height: 28,
+    margin: 4,
+  },
+  resize:{
+    fontSize:50
+  },
+}));
+
+export default function SearchBar() {
+  const classes = useStyles();
+
+  return (
+    <Paper component="form" className={classes.root}>
+     
+      <InputBase
+        className={classes.input}
+        placeholder="Search"
+        inputProps={{ 'aria-label': 'search google maps' , },{style: {fontSize: 40}}}
+        fullWidth
+      />
+      <IconButton type="submit" className={classes.iconButton} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  );
+}
