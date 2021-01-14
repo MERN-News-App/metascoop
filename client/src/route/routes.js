8644a742c4df27b1ed0a70c191a8f1c2848311d7
ben-rochlin <brochlin91@gmail.com>
2021-01-15 01:56:07 +1100

    mapped categories to routes

--- client/src/route/routes.js
+++ client/src/route/routes.js
@@ -3,13 +3,13 @@ import { Route, BrowserRouter, Switch } from "react-router-dom";
 
 import Home from "../pages/home";
 import CategoriesPage from "../pages/categoriesPage"
-// import CategoryPage from '../pages/category/categoryPage'
+import CategoryPage from '../pages/category/categoryPage'
 import PublicationsPage from "../pages/publicationsPage"
 import LoginPage from "../pages/loginPage"
 import SigninPage from "../pages/signinPage"
 import LatestNews from "../pages/latestPage"
 
-// import categories from './categoryRoute'
+import categories from './categoryRoute'
 
 import MenuBar from '../components/menuBar.jsx'
 import ContentCard from '../components/contentCard.jsx'
@@ -23,17 +23,21 @@ const Router = () => {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/categories" component={CategoriesPage} />
         <Route exact path="/publications" component={PublicationsPage} />
-        <Route exact path="/signin"  component={SigninPage} />
-        <Route exact path="/login"  component={LoginPage} />
-        <Route exact path="/latest-news"  component={LatestNews} />
-        <Route exact path="/newsApi"  component={NewsApi} />
-  
+        <Route exact path="/signin" component={SigninPage} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/latest-news" component={LatestNews} />
+        <Route exact path="/newsApi" component={NewsApi} />
 
-      {/* for checking out components */}
-        <Route exact path="/menubar"  component={MenuBar} />
-        <Route exact path="/contentCard" cd  component={ContentCard} />
+        <Route exact path="/categories" component={CategoriesPage} />
+        {categories.map((category) => {
+          <Route exact path={`/${category}`} exact component={CategoryPage} category={category} />
+        })}
+
+
+        {/* for checking out components */}
+        <Route exact path="/menubar" component={MenuBar} />
+        <Route exact path="/contentCard" cd component={ContentCard} />
       </Switch>
     </BrowserRouter>
   );
