4fbbc8275f2c63c9726ffca960a3067124d6ca41
ben-rochlin <brochlin91@gmail.com>
2021-01-15 01:22:59 +1100

    fixed sidebar bug that was not letting other components be clicked

--- client/src/components/sidebar.jsx
+++ client/src/components/sidebar.jsx
@@ -1,15 +1,25 @@
 import Sidebar from "react-sidebar";
 import SideBarContent from './sideBarContent.jsx'
+import './fix.scss'
 
 
 
 
 const styles = { 
     sidebar: { 
-        background: "white",
-        position: "fixed"
-
-    } 
+        position: "fixed",
+        background: "white"
+    },
+    root: {
+        position: "undefined"
+    },
+    content: {
+        position: "undefined",
+        top: "undefined",
+        left: "undefined",
+        right: "undefined",
+        bottom: "undefined"
+    }
 }
 
 const SIDE = ({ sidebarOpen, onSetSidebarOpen }) => {
@@ -21,7 +31,8 @@ const SIDE = ({ sidebarOpen, onSetSidebarOpen }) => {
             sidebar={<SideBarContent/>}
             open={sidebarOpen}
             onSetOpen={onSetSidebarOpen}
-            styles={styles}>
+            styles={styles}
+            >
                 <></>
         </Sidebar>
     )
