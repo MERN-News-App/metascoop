d7e83137dceae1d7cd05f55ab8daa768c2bb5972
ben-rochlin <brochlin91@gmail.com>
2021-01-16 13:12:55 +1100

    tweeks so would compile

--- client/src/pages/category/categoryPage.jsx
+++ client/src/pages/category/categoryPage.jsx
@@ -3,13 +3,14 @@ import MenuBar from '../../components/menuBar.jsx'
 import ContentCard from "../../components/contentCard.jsx";
 import Title from "../../components/title.jsx" 
 
-import ApiContext from '../../api/apiContext'
+// import ApiContext from '../../api/apiContext'
 
 
 
 const CategoryPage = () => {
 
-const category = useContext(ApiContext)
+// const category = useContext(ApiContext)
+const category = "food"
 
     const URL = () => {
         return `https://api.currentsapi.services/v1/search?category=${category}&apiKey=X2DqXEqyVN2kKq2QJPwiiZZ-xpRGdj7EEFCqpfDjv6CuX-fD` 
