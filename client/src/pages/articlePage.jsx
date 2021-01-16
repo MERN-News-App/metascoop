5c33d80d9534ec1a8001e343611f1e176d53d30f
ben-rochlin <brochlin91@gmail.com>
2021-01-16 12:06:03 +1100

    comments for clarity

--- client/src/pages/articlePage.jsx
+++ client/src/pages/articlePage.jsx
@@ -16,14 +16,18 @@ const ArticlePage = () => {
 //USE SELECTOR HERE
 
 
-
+//RETURN OBJECT.title HERE
 const category ="food"
+
+//RETURN OBJECT.apiURL HERE
 const URL = () => {
     return `https://api.currentsapi.services/v1/search?category=${category}&apiKey=X2DqXEqyVN2kKq2QJPwiiZZ-xpRGdj7EEFCqpfDjv6CuX-fD` 
 } 
 
 const apiURL = useState(URL)
 
+
+//WE WANT TO PASS THESE DOWN AS PROPS
 
     console.log("THIS ONE", URL)
     console.log("hello")
