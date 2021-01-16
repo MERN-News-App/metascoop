fc0b9d150fd59a2a5b7036a4256fddb397ddc629
ben-rochlin <brochlin91@gmail.com>
2021-01-16 23:05:34 +1100

    few changes, nothing major, think i need to seperate front and backend for .env file to work correctly

--- server/index.js
+++ server/index.js
@@ -1,23 +1,28 @@
+// require('dotenv').config()
+
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 import cors from 'cors';
 
 // import postRoutes from './routes/posts.js';
 
+//APP
 const app = express()
 
+//DB
+const CONNECTION_URL = 'mongodb+srv://Benjamin:Digitor123$@mernnews.hfgdh.mongodb.net/<dbname>?retryWrites=true&w=majority'
+mongoose.connect(process.env.DATABASE || CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Successfully connected to Mongoose Atlas"))
+    .catch((error) => console.log(error.message));
+mongoose.set('useFindAndModify', false);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
 app.use(cors())
 // app.use('/posts', postRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://Benjamin:Digitor123$@mernnews.hfgdh.mongodb.net/<dbname>?retryWrites=true&w=majority'
-const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`)))
-    .catch((error) => console.log(error.message));
 
-mongoose.set('useFindAndModify', false);
+const PORT = process.env.PORT || 6000;
+app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`))
