9c2b3461bdf0f8d4a49ebb21cee580e046396b50
ben-rochlin <brochlin91@gmail.com>
2021-01-16 14:04:37 +1100

    double did it, cleaned up code

--- client/src/pages/articlePage.jsx
+++ client/src/pages/articlePage.jsx
@@ -15,37 +15,19 @@ import Title from '../components/title.jsx'
 
 const ArticlePage = () => {
 
-
-//USE SELECTOR HERE
 const newsObj = useSelector((state) => state.newsObjects)
 console.log(newsObj.apiURL, "YOOOOOOOOO")
 
-
-//RETURN OBJECT.title HERE
-const category = useState(newsObj.title) 
-
-//RETURN OBJECT.apiURL HERE
-// const URL = () => {
-//     return `https://api.currentsapi.services/v1/search?category=${category}&apiKey=X2DqXEqyVN2kKq2QJPwiiZZ-xpRGdj7EEFCqpfDjv6CuX-fD` 
-// } 
-
-// const URL = newsObj.apiURL
-
+const title = useState(newsObj.title) 
 const apiURL = useState(newsObj.apiURL)
-
-
-//WE WANT TO PASS THESE DOWN AS PROPS
-
-    console.log("THIS ONE", URL)
-    // console.log("hello")
 
 
   return (
     <div>
       <MenuBar />
-      {/* // OBJECT.title */}
-      <Title category={category}/>
-      {/* OBJECT.apiURL */}
+      
+      <Title title={title}/>
+     
       <ContentCard2 apiURL={ apiURL }/>
     </div>
   );
