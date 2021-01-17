76777710efefc915e39694bc51e1db1ca2f02a80
ben-rochlin <brochlin91@gmail.com>
2021-01-18 02:21:42 +1100

    start on sidebar content

--- client/src/components/sideBarContent.jsx
+++ client/src/components/sideBarContent.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import './sideBarContent.scoped.scss'
 
+const name = "Ben"
+
 const SideBarContent = () => {
-  return(
-      <div className="outerContainer">
-      <h1>hello there</h1>
+  return (
+    <div className="outerContainer">
+      <div className="innerContainer">
+        <h1>hello there,  {name}</h1>
       </div>
+      <div className="line"></div>
+      <div className="linkDiv">
+        <div className="link">Latest News</div>
+        <div className="link">Publications</div>
+        <div className="link">Categories</div>
+      </div>
+      <div className="line"></div>
+      <div className="linkDiv">
+        <div className="link">Reading List</div>
+        <div className="link">Archive</div>
+      </div>
+      <div className="line"></div>
+      <div className="linkDiv">
+        <div className="link link2">Settings</div>
+      </div>
+
+    </div >
   )
 }
 
