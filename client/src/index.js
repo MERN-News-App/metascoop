9ade58300f7903d570d54a67e1dc86944d5ce150
ben-rochlin <brochlin91@gmail.com>
2021-01-18 19:16:16 +1100

    rubbish commit

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 
@@ -10,6 +9,7 @@ import reducers from './reducers'
 import App from './App'
 import './index.scss'
 
+import { createStore, applyMiddleware, compose } from 'redux';
 const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
 console.log(store.reducers, "HEEER I AM")
