13bd5ded4def451a88c4dca76d17780e9cd17d65
ben-rochlin <brochlin91@gmail.com>
2021-01-19 11:32:24 +1100

    responsive header

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -3,6 +3,7 @@ import './header.scoped.scss'
 
 const Header = () => {
   return(
+      <div className="spacer">
       <div className="outerContainer">
           <div className="item">
               <img src="latest-newsO.jpg" alt="" className="image"/>
@@ -16,6 +17,7 @@ const Header = () => {
               <img src="categoriesO.jpg" alt="" className="image"/>
               <span className="text">Categories</span>
           </div>   
+      </div>
       </div>
   )
 }
